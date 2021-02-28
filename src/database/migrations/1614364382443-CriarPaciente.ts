464c172a5b5fdd8f826ce0f1da547637b60208d0
Cleverton Bezerra <clev.inacio@gmail.com>
2021-02-28 18:14:42 -0300

    #4: Cria tabela pacientes no CreatePaciente

--- src/database/migrations/1614364382443-CriarPaciente.ts
+++ src/database/migrations/1614364382443-CriarPaciente.ts
@@ -1,11 +1,62 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { Column, MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CriarPaciente1614364382443 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table(
+                {
+                    name: "pacientes",
+                    columns: [
+                        {
+                            name: "id",
+                            type: "uuid",
+                            isPrimary: true
+                        },
+                        {
+                            name: "cpf",
+                            type: "varchar"
+                        },
+                        {
+                            name: "nsus",
+                            type: "varchar"
+                        },
+                        {
+                            name: "nome",
+                            type: "varchar"
+                        },
+                        {
+                            name: "cidade",
+                            type: "varchar"
+                        },
+                        {
+                            name: "bairro",
+                            type: "varchar"
+                        },
+                        {
+                            name: "numero",
+                            type: "int"
+                        },
+                        {
+                            name: "complemento",
+                            type: "varchar"
+                        },
+                        {
+                            name: "dtNascimento",
+                            type: "Date"
+                        },
+                        {
+                            name: "telefone",
+                            type: "varchar"
+                        }
+                    ]
+                }
+            )
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("pacientes");
     }
 
 }
