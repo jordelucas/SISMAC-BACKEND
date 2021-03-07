9c7bc0547af650662ec8be47a3aa2570881f7296
Cleverton Bezerra <clev.inacio@gmail.com>
2021-03-07 11:16:55 -0300

    #13: Cria teste de verificação de existência de Paciente

--- src/__tests__/Paciente.test.ts
+++ src/__tests__/Paciente.test.ts
@@ -27,22 +27,38 @@ describe("Pacientes", () => {
         expect(response.body).toHaveProperty("id");
     });
 
-    it("Should be able to get all Pacientes", async () => {
-        await request(app).post("/pacientes").send({
-            cpf: "1231234",
-            nsus: "2222222",
-            nome: "clevi",
+    it("Should not be able to create a new Pacient if it already exists", async () => {
+        const response = await request(app).post("/pacientes").send({
+            cpf: "123123123",
+            nsus: "111111111",
+            nome: "clev",
             cidade: "cang",
             bairro: "sertãozinho",
-            numero: "20",
+            numero: "25",
             complemento: "casa",
-            dtNascimento: "1998-10-30",
-            telefone: "8489498494"
+            dtNascimento: "1998-10-31",
+            telefone: "8494984499"
         });
 
-        const response = await request(app).get("/pacientes");
-
-        expect(response.body.length).toBe(9);
+        expect(response.status).toBe(400);
     })
+
+    // it("Should be able to get all Pacientes", async () => {
+    //     await request(app).post("/pacientes").send({
+    //         cpf: "1231234",
+    //         nsus: "2222222",
+    //         nome: "clevi",
+    //         cidade: "cang",
+    //         bairro: "sertãozinho",
+    //         numero: "20",
+    //         complemento: "casa",
+    //         dtNascimento: "1998-10-30",
+    //         telefone: "8489498494"
+    //     });
+
+    //     const response = await request(app).get("/pacientes");
+
+    //     expect(response.body.length).toBe(9);
+    // })
 
 });
