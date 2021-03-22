2a8c67e140fb65d169e8baf3b75d7d69cc6862f4
Cleverton Bezerra <clev.inacio@gmail.com>
2021-03-22 12:15:15 -0300

    #33: Cria .env e variável de ambiente para o banco

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,7 @@
 module.exports = [
     {
         "type": "postgres",
-        "url": "postgres://test:test@localhost:5432/database",
+        "url": process.env.DATABASE_URL,
         "synchronize": true,
         "logging": true,
         "migrations": [
@@ -27,5 +27,22 @@ module.exports = [
         "entities": [
             "./src/models/**.ts"
         ]
+    },
+    {
+        "name": "start",
+        "type": "postgres",
+        "url": process.env.DATABASE_URL,
+        "synchronize": true,
+        "logging": true,
+        "migrations": [
+            "build/src/database/migration/**.js"
+        ],
+        "entities": [
+            "build/src/models/**.js"
+        ],
+        "cli": {
+            "migrationsDir": "./src/database/migration",
+            "entitiesDir": "./src/models"
+        }
     }
 ]
