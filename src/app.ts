c16794aee373ee27ae207a4cffb214a9683aa706
Cleverton Bezerra <clev.inacio@gmail.com>
2021-03-25 09:18:48 -0300

    Adiciona types do PG e configura Cliente PG

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,28 @@ import express from 'express';
 import createConnection from "./database";
 import { router } from './routes';
 import cors from 'cors';
+import { Client } from 'pg';
 
 createConnection();
 
 const app = express();
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+    }
+    client.end();
+});
 
 app.use(cors())
 app.use(express.json());
